b75af947aee15c797df4a6036adf069d99173c3b
Benjmain Zwahlen <benzwahlen@gmail.com>
2025-10-07 22:30:32 +0100

    CS-472 Updated backup script

--- backup.sh
+++ backup.sh
@@ -13,7 +13,13 @@ mkdir -p backup_$SITE_ENV"_"$DATE/db/
 
 
 
-docker exec -t picklebook-db mariadb-dump -uroot -p${DB_ROOTPASSWORD} picklebook > backup_$SITE_ENV"_"$DATE/db/picklebook.sql
+if docker exec -t picklebook-db mariadb-dump -uroot -p${DB_ROOTPASSWORD} picklebook > backup_$SITE_ENV"_"$DATE/db/picklebook.sql ; then
+    echo "✅ Backup successful"
+else
+    echo "❌ Backup failed!"
+    exit 1
+fi
+
 
 cp .env backup_$SITE_ENV"_"$DATE/env
 cp -R /var/lib/docker/volumes/picklebook-user-uploads/_data/* backup_$SITE_ENV"_"$DATE/user-uploads/
